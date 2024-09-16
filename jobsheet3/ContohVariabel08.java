177748963c23a4ad669da6317456dc22f4be1847
Aryanzudaf <aryanzf1@gmail.com>
2024-09-16 10:56:45 +0700

    commit jobsheet 3

---
+++ jobsheet3/ContohVariabel08.java
@@ -0,0 +1,16 @@
+public class ContohVariabel08 {
+
+    
+}public static void main(String args[]) {
+    String hobby = "bermain layangan";
+    boolean isPandai = true;
+    char jenisKelamin = 'L';
+    byte _umurSayaSekarang = 20;
+    double $ipk = 3.24, tinggi = 1.78;
+        System.out.println("hobby saya adalah " + hobby);
+        System.out.println("apakah pandai" + isPandai);
+        System.out.println("jenis kelamin:" + jenisKelamin);
+        System.out.println("umurku saat ini:" + _umurSayaSekarang);
+        System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+} 
+
